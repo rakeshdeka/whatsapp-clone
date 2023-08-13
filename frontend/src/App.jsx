f6b68ce4e5ed0425836bfb435044f3814c3d8412
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-08-13 21:23:32 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar'
+import HomePage from './components/HomePage'
+
+function App() {
+
+
+  return (
+    <>
+
+      <HomePage />
+
+
+
+    </>
+  )
+}
+
+export default App
